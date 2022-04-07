aae874a7047637792f2dcd349244f55586d06f19
hemendco <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>
2022-04-07 22:51:49 +0430

    update interface

--- lib/interface.ts
+++ lib/interface.ts
@@ -2,7 +2,7 @@
 export interface PageTitleNotificationInterface {
     vars: {
         originalTitle: string;
-        interval: NodeJS.Timer|null;
+        interval: NodeJS.Timer | null;
     };
     On(notification: string, intervalSpeed: number) : void;
     Off() : void;
